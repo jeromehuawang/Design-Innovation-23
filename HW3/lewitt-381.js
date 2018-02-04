e13d54d2bd227880b33893f21065634fe2ed9058
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-03 18:14:51 -0800

    Create lewitt-381.js

---
+++ HW3/lewitt-381.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(300, 300);
+}  
+colorMode(HSB);
+
+function draw() {
+  noLoop();
+  noFill();
+	background(250);
+  noStroke();
+  fill(0,0,63)
+	rect(25,25,125,125)
+  fill(197,84,80)
+  rect(150,150,125,125)
+  fill(57,84,80)
+  rect(150,25,125,125)
+  fill(341,80,85)
+  rect(25,150,125,125)
+  
+}  
