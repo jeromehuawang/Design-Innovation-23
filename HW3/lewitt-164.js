4c9110dd36c1f1e3a56208571f9f305486a7923b
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-03 18:08:51 -0800

    Create lewitt-164.js

---
+++ HW3/lewitt-164.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(300, 300);
+}  
+colorMode(HSB);
+
+function draw() {
+  noLoop();
+  noFill();
+	background(28,12,98);
+	rect(25,25,250,250)
+  stroke(360,100,80)
+ 	line(50,150, 250,150)
+  line(100,200, 200,100)
+}  
