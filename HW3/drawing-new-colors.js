081eec68c1cf1d4e1f86705a0caf72aa2d050743
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-03 22:50:12 -0800

    Create drawing-new-colors.js

---
+++ HW3/drawing-new-colors.js
@@ -0,0 +1,29 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(255, 107, 107);
+  } else if (key == 'G') {
+    fill(133, 203, 51);
+  } else if (key == 'B') {
+    fill(179, 255, 252); 
+  } else if (key == 'Y') {
+    fill(255, 242, 117);
+	} else if (key == 'V') {
+    fill(157, 117, 203);
+	} else if (key == 'P') {
+    fill(255, 169, 184);
+}
+}
+	
