d7fa53cb1c38d9f4fbe0f9dbbf3cf0f98396015c
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-03 22:40:10 -0800

    Update concentric-circles.js

--- HW3/concentric-circles.js
+++ HW3/concentric-circles.js
@@ -1,7 +1,8 @@
+
 function draw() {
   background(250);
   noLoop();
-	for (var i = 300; i < width; i = i - 10) {
+	for (var i = 300; i >= 0 ; i = i - 10) {
   	var startX = width/2;
   	var startY = height/2;
   	var w= i
