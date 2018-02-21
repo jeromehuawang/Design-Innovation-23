da533aef39bc424b0f9d82fb47488c4cdea4fc10
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-20 23:20:05 -0800

    Update smoke-arrays.js

--- HW5/smoke-arrays.js
+++ HW5/smoke-arrays.js
@@ -1,47 +1,43 @@
-var x1 = [];
-var y1 = [];
-var d1 = [];
-
-var x2 = [];
-var y2 = [];
-var d2 = [];
+var x = [];
+var y = [];
+var r = [];
 
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB)
-	 for (i = 0; i < 50; i = i + 1){
-    x1[i] = random(0, 100);
-    y1[i] = random(0, 200);
-    d1[i] = random(0,100);
-    x2[i] = random(0, 300);
-    y2[i] = random(0, 200);
-    d2[i] = random(0,30); 
-
-  }  
+	for (i=0; i<150; i=i+1) {
+    x[i] = 200
+    y[i] = 250
+    r[i] = 0
+	}
 }
-
   
 function draw() {
-  background(255);
+  background(0);
   noStroke();
 
-  for (i = 0; i < 50; i = i + 1){
-    fill(random);
-    ellipse(x1[i], y1[i], d1[i]);
-    fil(random);
-    ellipse(x2[i], y2[i], d2[i]);
-
-  	
-  // 1% of the time
-  if (random() < 0.01) {
-    // random diameter between 10 and 400
-    d1[i] = random(10, 400);
-  }
+  // draw smokestack
+  fill(255);
+  rect(195, height, 30, -100);
+	for (i=0; i<100; i=i+3) {
+	y[i] = y[i] - random(0,3)
+	r[i] = r[i] + random(0,.05)
+  // save graphics state
+  push();
+  // darker as it gets closer to 0
+  fill(y[i]);
+  // rotate r around (x,y)
+  translate(x[i], y[i]);
+  rotate(r[i]);
+  // draw rectangle
+  rect(-10, -10, 20, 20);
+  // reset rotation and translation
+  pop();
   
-  // 2% of the time…
-  if (random() < 0.02) {
-    // random diameter between 10 and 400
-    d2[i] = random(10, 400);
-     }
+  // up 3 pixels
+  rect(-10, -10, 20, 20);
+  // if reach past the top a bunch
+  if (y < -150) {
+    y = 290;
+  }
   }
 }
