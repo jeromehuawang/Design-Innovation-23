4e17f5f2ea3bb2f01a1ed40b94298cb999001436
Jerome Wang <33115601+jeromehuawang@users.noreply.github.com>
2018-02-26 01:05:33 -0800

    Update bouncing-circles.js

--- HW6/bouncing-circles.js
+++ HW6/bouncing-circles.js
@@ -1,10 +1,11 @@
 var circles = [];
+numcircles=100;
 
 function setup() {
   createCanvas(400, 400);
   colorMode(HSB);
 
-  for (var index = 0; index < 100; index = index + 1) {
+  for (var index = 0; index < numcircles; index = index + 1) {
     // new "circle" object, with x, y, xd, yd, and c properties:
     circles[index] = {
       x: width / 2,
@@ -12,7 +13,7 @@ function setup() {
       xd: random(-2, 2),
       yd: random(-2, 2),
       c: color(random(360), 60, 100),
-      r: random(10,30)
+			r: random(5,20)
     }
   }
 }
@@ -21,7 +22,7 @@ function draw() {
   background(0);
   noStroke();
 
-  for (var index = 0; index < 1; index = index + 1) {
+  for (var index = 0; index < numcircles; index = index + 1) {
     // get circle object
     var circle = circles[index];
 
@@ -34,17 +35,22 @@ function draw() {
     circle.y = circle.y + circle.yd;
 
     // check boundaries and update directions
-    if (circle.r < 3) {
-       ellipse(circle.x, circle.y, circle.r);
+    if (circle.r <=2 || circle.r>30) {
+        circle.r = random(5,20);
     }
     if (circle.x > width || circle.x < 0) {
-      circle.r = circle.r/2
       circle.xd = -circle.xd;
-      }
+			circle.r = circle.r - random(1,3)
+			push();
+      text('boop', random(0,width), random(0,height));
+			pop();
     }
     if (circle.y > height || circle.y < 0) {
-      circle.r = circle.r/2
       circle.yd = -circle.yd;
+			circle.r = circle.r - random(1,3)
+			push();
+      text('boop', random(0,width), random(0,height));
+			pop();
     }
-
   }
+}
